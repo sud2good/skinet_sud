3a07117fc7fbb13572feeb418f9ccc2d222b81d3
sud2good <Zsudhanshu@gmail.com>
2020-09-11 16:45:33 +0530

    completed basic shopping app

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { IProduct } from './models/product';
-import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +8,18 @@ import { IPagination } from './models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: IProduct[];
+  // products: IProduct[];
 
   constructor(private http: HttpClient){
   }
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
-      (response: IPagination) => {
-      // console.log(response);
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    });
+    // this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
+    //   (response: IPagination) => {
+    //   // console.log(response);
+    //   this.products = response.data;
+    // }, error => {
+    //   console.log(error);
+    // });
   }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,18 +5,22 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CoreModule } from './core/core.module';
+import { ShopModule } from './shop/shop.module';
+// import { NavBarComponent } from './core/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+   // NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule,
+    ShopModule
   ],
   providers: [],
   bootstrap: [AppComponent]
